d8e5354b813541a170349a3986eec3680037db58
BaekDorazi <qorehgud@gmail.com>
2020-09-09 00:00:02 +0900

    기능 테스트 / 사이드 메뉴 테스트중

--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
-import { slide as Menu } from 'react-burger-menu'
+
 
 class Main extends Component {
     render() {
         return (
             <div>
                 <h1>리액트</h1>
-                <Menu>
-                    <a id="home" className="menu-item" href="/">Home</a>
-                    <a id="about" className="menu-item" href="/about">About</a>
-                    <a id="contact" className="menu-item" href="/contact">Contact</a>
-                    <a onClick={this.showSettings} className="menu-item--small" href="">Settings</a>
-                </Menu>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Main from './Main';
+import Sidebar from "./components/Sidebar"
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Main />
+    <Sidebar pageWrapId={"page-wrap"} outerContainerId={"App"} />
+
+    <div id="page-wrap">
+      <Main />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
