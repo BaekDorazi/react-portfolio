57489fd77251509b72c0dd52eb07703d927704c4
BaekDorazi <qorehgud@gmail.com>
2020-09-25 18:11:24 +0900

    기능추가, UI수정 / reactstrap 설치, 내정보 아이콘 눌렀을때 Modal 띄워주도록 추가(css 적용 x, 기능도 완벽 x)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,41 @@
 import React, { Component } from "react";
 import logoImg from "../../images/001-knight.png";
+import MyInfoModal from "../Modal/MyInfoModal";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { myInfoModalStatus: false };
+  }
+
+  handleClickLogo = () => {};
+
+  toggleModal = () => {
+    console.log("==toggleModal ");
+    this.setState({
+      myInfoModalStatus: !this.state.myInfoModalStatus,
+    });
+  };
+
   render() {
+    const { myInfoModalStatus } = this.state;
+
     return (
       <div className="abs header">
         <div className="logo-container">
           <img src={logoImg} width="45px" height="45px" />
         </div>
         <div className="menu">
-          <div className="my-info-icon">
+          <div className="my-info-icon" onClick={this.toggleModal}>
             <i className="icon-avatar" />
           </div>
           <div className="logout-icon">
             <i className="icon-logout" />
           </div>
         </div>
+
+        <MyInfoModal isOpen={myInfoModalStatus} toggle={this.toggleModal} />
       </div>
     );
   }
