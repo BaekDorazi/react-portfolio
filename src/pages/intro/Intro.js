8f3722b350fb1898dd50c664f40e991b71f52f91
BaekDorazi <qorehgud@gmail.com>
2020-09-30 18:10:53 +0900

    UI수정 / 폰트 색 노랑-> 빨강으로 변경, intro 화면에 이미지 추가

--- src/pages/intro/Intro.js
+++ src/pages/intro/Intro.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import introImg from "../../images/intro-img.png";
+// import logoImg from "../../images/001-knight.png";
 
 class Intro extends Component {
   constructor(props) {
@@ -24,13 +26,14 @@ class Intro extends Component {
           <div className="font-size-12pt first-line">Hi, my name is</div>
           <div className="font-size-30pt second-line">Yusunhee.</div>
           <div className="font-size-30pt third-line">
-            All I Wanna Do{" "}
-            <font className="yellow-font">Design&Programming.</font>
+            All I Wanna Do <font className="red-font">Design&Programming.</font>
           </div>
           <div className="font-size-10pt fourth-line">
             Web Design / Publisher
           </div>
         </div>
+
+        <img className="intro-image" src={introImg} height="65%" />
       </div>
     );
   }
