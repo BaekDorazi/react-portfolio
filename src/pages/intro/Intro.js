5252e8fced49087026d9490db1847c0fae2129be
BaekDorazi <qorehgud@gmail.com>
2020-10-05 18:35:00 +0900

    기능추가, UI수정 / text 타이핑 효과 라이브러리 추가. intro 화면에서 sample로 뿌림

--- src/pages/intro/Intro.js
+++ src/pages/intro/Intro.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import introImg from "../../images/intro-img.png";
 import bloodHandImg from "../../images/blood-hand.png";
+import Typing from "react-typing-animation";
 
 class Intro extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = { test: false };
   }
 
   static getDerivedStateFromProps(nextProps, prevState) {
@@ -19,10 +20,16 @@ class Intro extends Component {
 
   componentWillUnmount() {}
 
+  showCursor = () => {
+    console.log("zzz 찍히는 시점");
+    this.setState({ test: true });
+  };
+
   render() {
+    const { test } = this.state;
     return (
       <div className="intro-container">
-        <div className="intro-content">
+        {/* <div className="intro-content">
           <div className="font-size-12pt first-line">Hi, my name is</div>
           <div className="font-size-30pt second-line">Yusunhee.</div>
           <div className="font-size-30pt third-line">
@@ -37,6 +44,15 @@ class Intro extends Component {
           <img className="blood-hand-image1" src={bloodHandImg} height="7%" />
           <img className="blood-hand-image2" src={bloodHandImg} height="7%" />
           <img className="blood-hand-image3" src={bloodHandImg} height="7%" />
+        </div> */}
+        <div className="intro-content">
+          <Typing loop onFinishedTyping={this.showCursor}>
+            <Typing.Delay ms={2000} />
+            <div className="font-size-30pt second-line">
+              디자이너 유선희 포트폴리오입니다.
+              <Typing.Reset delay={10000} />
+            </div>
+          </Typing>
         </div>
       </div>
     );
