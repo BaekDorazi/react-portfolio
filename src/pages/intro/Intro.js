a3480e15103a60fbc724b59b6f06226c9fb8cc0a
BaekDorazi <qorehgud@gmail.com>
2020-10-07 18:42:14 +0900

    UI수정 / intro 배경 및 내용 변경

--- src/pages/intro/Intro.js
+++ src/pages/intro/Intro.js
@@ -7,7 +7,7 @@ class Intro extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { test: false };
+    this.state = {};
   }
 
   static getDerivedStateFromProps(nextProps, prevState) {
@@ -20,37 +20,20 @@ class Intro extends Component {
 
   componentWillUnmount() {}
 
-  showCursor = () => {
-    console.log("zzz 찍히는 시점");
-    this.setState({ test: true });
-  };
-
   render() {
-    const { test } = this.state;
+    const { userNum } = this.props;
+
     return (
       <div className="intro-container">
-        {/* <div className="intro-content">
-          <div className="font-size-12pt first-line">Hi, my name is</div>
-          <div className="font-size-30pt second-line">Yusunhee.</div>
-          <div className="font-size-30pt third-line">
-            All I Wanna Do <font className="red-font">Design&Programming.</font>
-          </div>
-          <div className="font-size-10pt fourth-line">
-            Web Design / Publisher
-          </div>
-        </div>
-        <div className="intro-image-area">
-          <img className="intro-image" src={introImg} height="55%" />
-          <img className="blood-hand-image1" src={bloodHandImg} height="7%" />
-          <img className="blood-hand-image2" src={bloodHandImg} height="7%" />
-          <img className="blood-hand-image3" src={bloodHandImg} height="7%" />
-        </div> */}
         <div className="intro-content">
-          <Typing loop onFinishedTyping={this.showCursor}>
-            <Typing.Delay ms={2000} />
-            <div className="font-size-30pt second-line">
-              디자이너 유선희 포트폴리오입니다.
-              <Typing.Reset delay={10000} />
+          {/* <Typing loop onFinishedTyping={this.showCursor}> */}
+          <Typing onFinishedTyping={this.showCursor}>
+            <Typing.Delay ms={0} />
+            <div className="font-size-50pt">
+              {userNum === 1
+                ? "Developer 백도형 포트폴리오입니다."
+                : "디자이너 유선희 포트폴리오입니다."}
+              {/* <Typing.Reset delay={10000} /> */}
             </div>
           </Typing>
         </div>
