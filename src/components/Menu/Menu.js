0fb25613779663f8885a5a21fbe300126c8eb841
BaekDorazi <qorehgud@gmail.com>
2020-10-10 13:38:55 +0900

    UI추가, 기능추가 / 탑메뉴 추가

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ReactFullpage from "@fullpage/react-fullpage";
 
 class Menu extends Component {
   constructor(props) {
@@ -12,7 +13,23 @@ class Menu extends Component {
   render() {
     const { myInfoModalStatus } = this.state;
 
-    return <div></div>;
+    return (
+      <div>
+        <ReactFullpage
+          render={({ state, fullpageApi }) => {
+            return (
+              <div style={{ display: "flex" }}>
+                <div onClick={() => fullpageApi.moveTo(1, 0)}>Intro</div>
+                <div onClick={() => fullpageApi.moveTo(2, 0)}>Profile</div>
+                <div onClick={() => fullpageApi.moveTo(3, 0)}>Career</div>
+                <div onClick={() => fullpageApi.moveTo(4, 0)}>Skill</div>
+                <div onClick={() => fullpageApi.moveTo(5, 0)}>Portfolio</div>
+              </div>
+            );
+          }}
+        />
+      </div>
+    );
   }
 }
 
