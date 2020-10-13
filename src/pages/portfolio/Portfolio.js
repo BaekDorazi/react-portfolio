bdd650656ae52df5ac5d5723d34d2e1be000ff4d
YUSUNHEE\YUSUNHEE <yusunhee_@naver.com>
2020-10-13 18:04:35 +0900

    UI수정 / 포트폴리오 애니메이션 추가, 프로필 css 추가, 스킬 배경화면 색상 변경

--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -3,7 +3,15 @@ import ContentRect from "../../components/common/ContentRect";
 
 class Portfolio extends Component {
   render() {
-    return <div className="portfolio-container"></div>;
+    return (
+      <div className="portfolio-container">
+        <div className="loader">
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -6,9 +6,7 @@ class Profile extends Component {
   render() {
     return (
       <div className="profile-container">
-        <div className="intro-image-area">
-          <img className="profile-sunhee" src={Sunhee} height="30%" />
-        </div>
+          <img className="sunhee-img" src={Sunhee} />
       </div>
     );
   }
