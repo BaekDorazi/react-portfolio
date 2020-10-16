fd1c3660eee9d50e0a84988e2a80a0344a62ae9a
BaekDorazi <qorehgud@gmail.com>
2020-10-16 16:21:27 +0900

    UI수정 / profile 배경색상 변경, profile image 변경

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import ContentRect from "../../components/common/ContentRect";
-import Sunhee from "../../images/sunhee_white.png";
+import profileImg from "images/profile_img.png";
 
 class Profile extends Component {
   render() {
     return (
       <div className="profile-container">
-          <img className="sunhee-img" src={Sunhee} />
+        <img className="profile-img" src={profileImg} />
       </div>
     );
   }
