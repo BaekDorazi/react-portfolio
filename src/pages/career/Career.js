86d085493a5d7c65b918c539c3a55fa66eb10e51
BaekDorazi <qorehgud@gmail.com>
2020-11-19 23:37:07 +0900

    UI수정 / career 페이지의 디자인 변경 및 자격증 영역, 스킬 영역 디자인 변경

--- src/pages/career/Career.js
+++ src/pages/career/Career.js
@@ -14,98 +14,100 @@ class Career extends Component {
 
     return (
       <div className="career-container">
-        <div className="license-container">
-          <Timeline lineColor={"#ffffff"}>
-            <TimelineItem dateText="2014.01">
-              <h5>자격증명</h5>
-              <p>발급기관</p>
-            </TimelineItem>
-            <TimelineItem dateStyle={{ display: "none" }}>
-              <h5>자격증명</h5>
-              <p>발급기관</p>
-            </TimelineItem>
-            <TimelineItem dateText="2020.10">
-              <h5>자격증명</h5>
-              <p>발급기관</p>
-            </TimelineItem>
-            <TimelineItem dateStyle={{ display: "none" }}>
-              <h5>자격증명</h5>
-              <p>발급기관</p>
-            </TimelineItem>
-            <TimelineItem dateText="2020.10">
-              <h5>유선희</h5>
-              <p>백도형</p>
-            </TimelineItem>
-            <TimelineItem dateText="2020.11">
-              <h5>자격증명</h5>
-              <p>발급기관</p>
-            </TimelineItem>
-            <TimelineItem dateStyle={{ display: "none" }}>
-              <h5>자격증명</h5>
-              <p>발급기관</p>
-            </TimelineItem>
-          </Timeline>
+        <div className="center-rect"></div>
+        <div className="license-skill-container">
+          <div className="license-container">
+            <Timeline lineColor={"#ffffff"}>
+              <TimelineItem dateText="2014.01">
+                <h5>자격증명</h5>
+                <p>발급기관</p>
+              </TimelineItem>
+              <TimelineItem dateStyle={{ display: "none" }}>
+                <h5>자격증명</h5>
+                <p>발급기관</p>
+              </TimelineItem>
+              <TimelineItem dateText="2020.10">
+                <h5>자격증명</h5>
+                <p>발급기관</p>
+              </TimelineItem>
+              <TimelineItem dateStyle={{ display: "none" }}>
+                <h5>자격증명</h5>
+                <p>발급기관</p>
+              </TimelineItem>
+              <TimelineItem dateText="2020.10">
+                <h5>유선희</h5>
+                <p>백도형</p>
+              </TimelineItem>
+              <TimelineItem dateText="2020.11">
+                <h5>자격증명</h5>
+                <p>발급기관</p>
+              </TimelineItem>
+              <TimelineItem dateStyle={{ display: "none" }}>
+                <h5>자격증명</h5>
+                <p>발급기관</p>
+              </TimelineItem>
+            </Timeline>
+          </div>
+          <div className="skill-container">
+            <div className="left-chart-container">
+              <SkillProgress
+                isPageFocus={isPageFocus}
+                percentVal={70}
+                color={"#123456"}
+                centerSkillName="PS"
+                skillName="Photoshop"
+              />
+
+              <SkillProgress
+                isPageFocus={isPageFocus}
+                percentVal={30}
+                color={"#ab5482"}
+                centerSkillName="AI"
+                skillName="Illustrator"
+              />
+
+              <SkillProgress
+                isPageFocus={isPageFocus}
+                percentVal={90}
+                color={"#855124"}
+                centerSkillName="ID"
+                skillName="Indesign"
+              />
+            </div>
+            <div className="right-chart-container">
+              <SkillProgress
+                isPageFocus={isPageFocus}
+                percentVal={70}
+                color={"#123456"}
+                centerSkillName="AE"
+                skillName="After Effects"
+              />
+
+              <SkillProgress
+                isPageFocus={isPageFocus}
+                percentVal={30}
+                color={"#ab5482"}
+                centerSkillName="PR"
+                skillName="Premier"
+              />
+
+              <SkillProgress
+                isPageFocus={isPageFocus}
+                percentVal={90}
+                color={"#855124"}
+                centerSkillName="H/C"
+                skillName="HTML+CSS"
+              />
+            </div>
+          </div>
         </div>
 
-        <div className="skill-container">
-          <div className="left-chart-container">
-            <SkillProgress
-              isPageFocus={isPageFocus}
-              percentVal={70}
-              color={"#123456"}
-              centerSkillName="PS"
-              skillName="Photoshop"
-            />
-
-            <SkillProgress
-              isPageFocus={isPageFocus}
-              percentVal={30}
-              color={"#ab5482"}
-              centerSkillName="AI"
-              skillName="Illustrator"
-            />
-
-            <SkillProgress
-              isPageFocus={isPageFocus}
-              percentVal={90}
-              color={"#855124"}
-              centerSkillName="ID"
-              skillName="Indesign"
-            />
-          </div>
-          <div className="right-chart-container">
-            <SkillProgress
-              isPageFocus={isPageFocus}
-              percentVal={70}
-              color={"#123456"}
-              centerSkillName="AE"
-              skillName="After Effects"
-            />
-
-            <SkillProgress
-              isPageFocus={isPageFocus}
-              percentVal={30}
-              color={"#ab5482"}
-              centerSkillName="PR"
-              skillName="Premier"
-            />
-
-            <SkillProgress
-              isPageFocus={isPageFocus}
-              percentVal={90}
-              color={"#855124"}
-              centerSkillName="H/C"
-              skillName="HTML+CSS"
-            />
-          </div>
-        </div>
-
-        <div className="animation-container">
+        {/* <div className="animation-container">
           <div className="dot"></div>
           <div className="step s1"></div>
           <div className="step s2"></div>
           <div className="step s3"></div>
-        </div>
+        </div> */}
       </div>
     );
   }
