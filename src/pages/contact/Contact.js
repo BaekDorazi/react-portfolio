81e2c17b7badea6218e547a28c9bcbb9734e8fbd
YUSUNHEE\YUSUNHEE <yusunhee_@naver.com>
2020-12-09 16:03:16 +0900

    UI 수정 / 콘택트 디자인중

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
+import profileImg from "../../images/profile_sh.jpg";
+import SunheeImg from "../../images/sunhee.jpg";
 
 class Contact extends Component {
   render() {
     return (
       <div className="contact-container">
-        <div className="test">"Contact me"</div>
+          
+        <ul>
+          <li>
+            <div class="image">
+              <img className="profile-img" src={profileImg} />
+            </div>
+            <div class="shadow"></div>
+          </li>
+          <li>
+            <div class="image">
+              <img className="profile-img" src={SunheeImg} />
+            </div>
+            <div class="shadow"></div>
+          </li>
+          <li>
+            <div class="image">
+              <img src="https://unsplash.it/300/200?image=433"></img>
+            </div>
+            <div class="shadow"></div>
+          </li>
+        </ul>
       </div>
     );
   }
